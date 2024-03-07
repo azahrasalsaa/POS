b3ea308088906d845e2d2347ee8deb61028a27c7
AzahraSalsaa <azahras751@gmail.com>
2024-03-07 16:49:04 +0700

    first commit

---
+++ resources/views/beauty_health.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Beauty & Health</title>
+</head>
+<body>
+    <h1>Beauty & Health Category</h1>
+</body>
+</html>
---
+++ resources/views/food_beverage.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food & Beverage</title>
+</head>
+<body>
+    <h1>Food & Beverage Category</h1>
+</body>
+</html>
